b7c513082eb85ccbc54c33a69a1a527c43debcc5
derekdeming <derekdeming17@gmail.com>
2023-12-06 23:45:47 -0500

    l;dksjf

--- python/day5/main.py
+++ python/day5/main.py
@@ -7,13 +7,22 @@ derek_the_turtle.color("red")
 def square():
     for _ in range(4):
         derek_the_turtle.forward(100)
+        dash_line()
         derek_the_turtle.right(90)
-        
+
+def dash_line():
+    for _ in range(5):
+        derek_the_turtle.forward(10)
+        derek_the_turtle.penup()
+        derek_the_turtle.forward(10)
+        derek_the_turtle.pendown()
+
 square()
-
-
-
-
 
 screen = Screen()
 screen.exitonclick()
+        
+        
+        
+
+# dash_line()
