817f6f7f05540a60227377fb9394d2005fb1801c
derekdeming <derekdeming17@gmail.com>
2023-12-06 23:56:35 -0500

    day 5 complete

--- python/day5/main.py
+++ python/day5/main.py
@@ -16,8 +16,18 @@ def dash_line():
         derek_the_turtle.penup()
         derek_the_turtle.forward(10)
         derek_the_turtle.pendown()
+        
 
-square()
+def multi_shape():
+    colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "gray"]
+    for sides in range(3, 11):
+        derek_the_turtle.color(colors[sides-3])
+        for _ in range(sides):
+            derek_the_turtle.forward(100)
+            derek_the_turtle.right(360 / sides)
+                
+
+multi_shape()
 
 screen = Screen()
 screen.exitonclick()
