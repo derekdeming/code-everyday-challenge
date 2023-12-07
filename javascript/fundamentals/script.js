941bdb928a58add1b855464f6ce3e50fa1cee867
Derek <derekdeming17@gmail.com>
2023-12-07 18:53:32 -0500

    javascript

--- javascript/fundamentals/script.js
+++ javascript/fundamentals/script.js
@@ -115,7 +115,7 @@ const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
 
 ////////////////////////////////////
-*/
+
 
 const firstName = "derek"
 const job = "software engineer"
@@ -126,3 +126,104 @@ const derek = "I'm " + firstName + ', a ' + (curr_year-birthYear) + ' years old 
 console.log(derek)  
 
 const derekNew = `I'm ${firstName}, a ${curr_year-birthYear} years old ${job}!`;
+
+
+
+const age = 15
+
+
+if (age >= 18) {
+    console.log("Sarah can start driving license 🚗")
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+}
+
+// control structure for if else statements: 
+
+// if (condition) {
+//     // code
+// }
+// else if (another condition) {
+//     // code
+// } 
+
+// const birthYear = 1991
+// let century; 
+// if (birthYear <= 2000) {
+//     let century = 20; 
+// } else {
+//     let century = 21;
+// }
+// console.log(century)
+
+////////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion -- manually change type from one to another 
+
+const inputYear = '1991';
+console.log(inputYear + 18);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Derek'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion -- javascript automatically change type from one to another
+
+console.log('I am ' + 23 + ' years old'); // + operator turns numbers into strings 
+console.log('23' - '10' - 3); // - operator turns strings into numbers 
+
+console.log('23' * '2'); // converts them to numbers 
+console.log('23' / '2'); // converts to numbers 
+
+
+// truthy and falsy values 
+// 5 falsy values: 0, '', undefined, null, NaN -- they will be converted to false when we attempt to convert them into a boolean 
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Derek'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+console.log(!null);
+console.log(!'Derek');
+console.log(!0);
+
+const money = 0 
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job"); // this converts the if condition to false so it runs the else statement (falsey value) 
+}
+
+*/
+////////////////////////////////////
+// Equality Operators: == vs. ===
+
+const age = '18'
+if (age === 18) console.log('you just became an adult (strict -- does not perform type coersion)')
+
+if (age == 18) console.log("you just became an adult (loose -- it does perform type coersion meaning '18' == 18)")
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite == 23) {
+    console.log('Cool! 23 is an amazing number!')
+}
+
+if (favorite === 23) {
+    console.log('Cool! 23 is an amazing number!')
+} else if (favorite === 8) {
+    console.log('8 is also a cool number')
+} else {
+    console.log('Number is not 23 or 8')
+}
+
+if (favorite !== 23) console.log('Why not 23?') // strict (!==) and loose (!=) -- always use the strict version 
+
+*/
