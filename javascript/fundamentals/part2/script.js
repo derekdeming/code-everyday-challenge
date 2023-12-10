587989b33ef65cb77fa1c6cc878e29878806533d
Derek <derekdeming17@gmail.com>
2023-12-10 14:08:58 -0500

    coding challenge complete

--- javascript/fundamentals/part2/script.js
+++ javascript/fundamentals/part2/script.js
@@ -339,3 +339,46 @@ console.log(jonas.getSummary());
 ///////////////////////////////////////
 // Coding Challenge #3
 
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi
+    }
+} 
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi
+    }
+}
+
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+      this.bmi = this.mass / (this.height * this.height);
+      return this.bmi;
+    }
+  };
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
+
+console.log(mark.calcBMI(), john.calcBMI())
