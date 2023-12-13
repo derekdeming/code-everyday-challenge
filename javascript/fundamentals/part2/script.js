6ed2f8f9fbc10a9fbc168bbc0e73b43d4de11480
derekdeming <derekdeming17@gmail.com>
2023-12-12 22:45:52 -0500

    loop comprehensions

--- javascript/fundamentals/part2/script.js
+++ javascript/fundamentals/part2/script.js
@@ -446,3 +446,28 @@ for (let i = 0; i < jonas.length; i++) {
 }
 
 */
+
+///////////////////////////////////////
+// Looping Backwards and Loops in Loops
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i])
+}
+
+// looping inside of a loop
+
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-------- Starting exercise ${exercise} --------`)
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`)
+    }
+}
