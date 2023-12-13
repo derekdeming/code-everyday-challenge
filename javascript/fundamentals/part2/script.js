24b25a6e8ee9133e57995cd8d94458d1da889f71
derekdeming <derekdeming17@gmail.com>
2023-12-12 22:33:25 -0500

    loops, continue, break

--- javascript/fundamentals/part2/script.js
+++ javascript/fundamentals/part2/script.js
@@ -372,11 +372,77 @@ if (mark.bmi > john.bmi) {
 
 console.log(mark.calcBMI(), john.calcBMI())
 
-*/
+
 
 ///////////////////////////////////////
 // Iteration: The for Loop
 // for loop keeps running while condition is TRUE
 
+console.log('Lifting weights repetition 1 🏋️‍♀️')
+console.log('Lifting weights repetition 2 🏋️‍♀️')
+console.log('Lifting weights repetition 3 🏋️‍♀️')
+console.log('Lifting weights repetition 4 🏋️‍♀️')
+console.log('Lifting weights repetition 5 🏋️‍♀️')
+console.log('Lifting weights repetition 6 🏋️‍♀️')
+console.log('Lifting weights repetition 7 🏋️‍♀️')
+console.log('Lifting weights repetition 8 🏋️‍♀️')
+console.log('Lifting weights repetition 9 🏋️‍♀️')
+console.log('Lifting weights repetition 10 🏋️‍♀️')
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
+} // rep = rep + 1
+// for loop structure is made up of 3 parts: 1) initial value of the counter variable, 2) condition that is evaluated before each iteration, 3) counter update after each iteration
 
 
+
+///////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = []
+
+for (let i = 0; i < jonas.length; i++) {
+    // reading from jonas array
+    console.log(jonas[i], typeof jonas[i])
+
+    // filling types array -- method 1 
+    types[i] = typeof jonas[i]
+    // filling types array -- method 2 -- better 
+    types.push(typeof jonas[i])
+}
+console.log(types)
+
+const years = [1991, 2007, 1969, 2020]
+const ages = []
+for (let i = 0 ; i < years.length; i++) {
+    ages.push(2037 - years[i])
+}
+console.log(ages)
+
+// continue and break
+// continue exits the current iteration of the loop and continues to the next one
+// break completely terminates the whole loop
+
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue; // if the element is not a string, then continue to the next iteration
+    console.log(jonas[i], typeof jonas[i])
+}
+console.log(types)
+console.log('--- ONLY STRINGS ---')
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break; // if the element is a number, then break out of the loop
+    console.log(jonas[i], typeof jonas[i])
+}
+
+*/
