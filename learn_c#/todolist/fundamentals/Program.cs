cb368e59189d53667d967037af98c603edc3a498
Derek <derekdeming17@gmail.com>
2023-12-28 13:39:27 -0500

    coding exercise

--- learn_c#/todolist/fundamentals/Program.cs
+++ learn_c#/todolist/fundamentals/Program.cs
@@ -565,3 +565,33 @@ int FindMax(int[,] numbers)
     return max;
 }
 
+
+
+//###################
+//CODING EXERCISE: 
+//###################
+
+List<string> GetOnlyUpperCaseWords(List<string> words)
+{
+    var result = new List<string>();
+    foreach (var word in words)
+    {
+        if (result.Contains(word))
+        {
+            continue;
+        }
+        bool isWordUpperCase = true;
+        foreach(char letter in word)
+        {
+            if(!char.IsUpper(letter))
+            {
+                isWordUpperCase = false;
+            }
+        }
+        if(isWordUpperCase)
+        {
+            result.Add(word);
+        }
+    }
+    return result;
+}
