9072db5ef614ee682bb251c7cfa30151aef12abb
Derek <derekdeming17@gmail.com>
2023-12-27 23:34:35 -0500

    muti-dimensional arrays

--- learn_c#/todolist/fundamentals/Program.cs
+++ learn_c#/todolist/fundamentals/Program.cs
@@ -442,3 +442,37 @@ string BuildHelloString()
     }
     return result;
 }
+
+
+//###################
+//Multi-dimensional arrays
+//###################
+
+char[,] letters = new char[2, 3];
+letters[0, 0] = 'A';
+letters[0, 1] = 'B';
+letters[0, 2] = 'C';
+letters[1, 0] = 'D';
+letters[1, 1] = 'E';
+letters[1, 2] = 'F';
+
+
+var letters2 = new char[,]
+{
+    {'A', 'B','C' },
+    {'D', 'E','F' },
+};
+
+var height = letters.GetLength(0);
+var width = letters.GetLength(1);
+
+for (var i =  0; i < height; i++)
+{
+    Console.WriteLine($"i is {i}");
+
+    for (int j = 0; j < width; j++)
+    {
+        Console.WriteLine($"j is {j}");
+        Console.WriteLine($"element is {letters[i,j]}");
+    }
+}
