ef4f5af3ba9820251461cb6ad8258502e6763c99
Derek <derekdeming17@gmail.com>
2023-12-28 22:09:13 -0500

    coding exercise done

--- learn_c#/todolist/OOPBasics/codingExercises.cs
+++ learn_c#/todolist/OOPBasics/codingExercises.cs
@@ -104,6 +104,30 @@ namespace OOPBasics
     //####################
     //## CODING EXERCISE
     //####################    
+    public class Order
+    {
+        public string Item { get; }
+        private DateTime _date; 
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                if (value.Year == DateTime.Now.Year) 
+                { 
+                    _date = value; 
+                }
+            }
+        }
+        public Order(string item, DateTime date)
+        {
+            Item = item;
+            Date = date;
+        }
+    }
+
+
+
 
     //####################
     //## CODING EXERCISE
