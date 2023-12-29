9bded14e32c9a4292c163eae9766db436eda6e8b
Derek <derekdeming17@gmail.com>
2023-12-28 19:05:57 -0500

    coding exercise

--- learn_c#/todolist/OOPBasics/codingExercises.cs
+++ learn_c#/todolist/OOPBasics/codingExercises.cs
@@ -34,9 +34,29 @@ namespace OOPBasics
     }
 
     //####################
-    //## CODING EXERCISE
+    //## CODING EXERCISE -- TRIANGLE CLASS 
     //####################
+    public class Triangle
+    {
+        private int _base;
+        private int _height;
 
+        public Triangle(int @base, int height)
+        {
+            _base = @base;
+            _height = height;
+        }
+
+        public int CalculateArea()
+        {
+            return (_base * _height)/2;
+        }
+
+        public string AsString()
+        {
+            return $"Base is {_base}, height is {_height}";
+        }
+    }
 
 
 
