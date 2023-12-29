a7e40beb7d16a4114dd258e3fe542d4343446862
Derek <derekdeming17@gmail.com>
2023-12-28 19:18:24 -0500

    method overloading

--- learn_c#/todolist/OOPBasics/Program.cs
+++ learn_c#/todolist/OOPBasics/Program.cs
@@ -101,6 +101,34 @@ class ShapesMeasurementsCalculator
  * 
  */
 
-//Console.WriteLine("Height is " + rectangle1.GetHeight());
-//Console.WriteLine("Area is " + rectangle1.CalculateArea());
-//Console.WriteLine("Circumference is " + rectangle1.CalculateCircumference());
+
+
+class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+    }
+
+    public void OverwriteMonthAndDay(int month, int day) // using method overloading (this is okay as long as you can tell one method from another based on type, order or count of parameters, naming the method the same is fine 
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+
+    public void MoveByMonthsAndDays(int monthToAdd, int daysToAdd)
+    {
+        _date = new DateTime(
+            _date.Year, 
+            _date.Month + monthToAdd, 
+            _date.Day + daysToAdd);
+    }
+}
