6e6134da88f1317eb6e0d3809119ad7a2a0d94ae
Derek <derekdeming17@gmail.com>
2023-12-30 12:28:43 -0500

    static class coding exercise

--- learn_c#/todolist/OOPBasics/codingExercises.cs
+++ learn_c#/todolist/OOPBasics/codingExercises.cs
@@ -158,8 +158,33 @@ namespace OOPBasics
 
 
     //####################
-    //## CODING EXERCISE
+    //## CODING EXERCISE -- static classes - NumberToDayOfWeekTranslator
     //####################    
+    public static class NumberToDayOfWeekTranslator
+    {
+        public static string Translate(int number)
+        {
+        switch (number)
+        {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return "Invalid day of the week";
+        }
+        }
+    }
 
 
 
@@ -170,22 +195,20 @@ namespace OOPBasics
 
 
 
-}    //####################
+}   //####################
     //## CODING EXERCISE
     //####################
 
 
 
 
-}    //####################
-    //## CODING EXERCISE
-    //####################
+//####################
+//## CODING EXERCISE
+//####################
 
 
 
 
-
-}    //####################
-    //## CODING EXERCISE
-    //####################
-}
+//####################
+//## CODING EXERCISE
+//####################
