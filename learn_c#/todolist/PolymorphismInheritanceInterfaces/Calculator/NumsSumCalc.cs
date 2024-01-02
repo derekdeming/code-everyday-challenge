2bcb536b67ba6ed815795d039e2d6f20a951c715
Derek <derekdeming17@gmail.com>
2024-01-01 21:42:54 -0500

    virtual methods exercise

---
+++ learn_c#/todolist/PolymorphismInheritanceInterfaces/Calculator/NumsSumCalc.cs
@@ -0,0 +1,41 @@
+﻿
+using PolymorphismInheritanceInterfaces.Calculator;
+
+var numbers = new List<int> { 1, 4, 6, -1, 12, 44, -8, -19 };
+bool shallAddPositiveOnly = true;
+
+NumbersSumCalculator calculator = 
+    shallAddPositiveOnly ? 
+    new PositiveNumbersSumCalculator() :
+    new NumbersSumCalculator();
+
+int sum = calculator.Calculate(numbers);
+
+//sum = new NumbersSumCalculator().Calculate(numbers);
+Console.WriteLine("Sum is: " + sum);
+Console.ReadLine();
+
+namespace PolymorphismInheritanceInterfaces.Calculator
+{
+    public class NumbersSumCalculator
+
+
+    {
+        public int Calculate(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                if (ShallBeAdded(number))
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
+        protected virtual bool ShallBeAdded(int number)
+        {
+            return true;
+        }
+    }
+}
---
+++ learn_c#/todolist/PolymorphismInheritanceInterfaces/Calculator/PositiveNumSumCalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismInheritanceInterfaces.Calculator
+{
+    public class PositiveNumbersSumCalculator : NumbersSumCalculator
+    {
+        protected override bool ShallBeAdded(int number)
+        {
+            return number > 0;
+        }
+    }
+}
