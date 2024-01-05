bbf2421ebdfef7cc758ecd4b8f058a6f7837665c
Derek <derekdeming17@gmail.com>
2024-01-04 23:27:57 -0500

    interfaces

--- learn_c#/todolist/PolymorphismInheritanceInterfaces/Program.cs
+++ learn_c#/todolist/PolymorphismInheritanceInterfaces/Program.cs
@@ -160,3 +160,25 @@ public static class ListExtensions
         return result;
     }
 }
+
+//Interfaces - Applying multiple transformations to a number
+public static class InterfacesApplyingMutipleTransformationsToNumberExercise
+{
+    public static int Transform(
+        int number)
+    {
+        var transformations = new List<INumericTransformation>
+            {
+                new By1Incrementer(),
+                new By2Multiplier(),
+                new ToPowerOf2Raiser()
+            };
+
+        var result = number;
+        foreach (var transformation in transformations)
+        {
+            result = transformation.Transform(result);
+        }
+        return result;
+    }
+}
