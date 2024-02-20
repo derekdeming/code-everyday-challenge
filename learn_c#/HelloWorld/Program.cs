e6ebe6da4901a854ab995584d13f76b36999085e
Derek <derekdeming17@gmail.com>
2024-02-19 23:18:49 -0500

    program added

---
+++ learn_c#/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+
+            Console.ReadKey();
+        }
+    }
+}
