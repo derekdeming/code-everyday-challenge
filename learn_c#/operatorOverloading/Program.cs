fdbb392b6f7a25113d776f1c611ea39d17c9df02
Derek <derekdeming17@gmail.com>
2024-04-30 22:31:06 -0400

    new

---
+++ learn_c#/operatorOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; 
+
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        public class Rectangle
+        {
+            public int Width = 0;
+            public int Height = 0;
+            public Rectangle( int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public static Rectangle operator +(Rectangle rect1, Rectangle rect2)
+            {
+                Rectangle rectResult = new Rectangle(rect1.Width + rect2.Width, rect1.Height + rect2.Height);
+
+                return rectResult;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle(12, 10);
+            Rectangle rect2 = new Rectangle(5, 2);
+
+            Rectangle rectResult = rect1 + rect2;
+
+            Console.WriteLine((rectResult.Width, rectResult.Height));
+        }
+    }
+}
