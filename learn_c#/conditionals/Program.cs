4cd579b12bf42cbdd1ac4ed43cd0768325c67013
Derek <derekdeming17@gmail.com>
2024-04-30 23:07:26 -0400

    conditionals

---
+++ learn_c#/conditionals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IfStatementExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal moneyAmountInSavingsAccount = 600;
+            decimal costOfBike = 1000; 
+            bool canGetLoan = true;
+            decimal maxLoanAmount = 500;
+
+            if (moneyAmountInSavingsAccount >= costOfBike)
+            {
+                Console.WriteLine("I can buy the bike");
+            }
+            else if (canGetLoan && maxLoanAmount + moneyAmountInSavingsAccount >= costOfBike)
+            {
+                Console.WriteLine("I don't have enough in my savings account but i can get a loan");
+            }
+            else if (canGetLoan && moneyAmountInSavingsAccount + maxLoanAmount > costOfBike / 2) 
+            {
+                Console.WriteLine("I have over half the money to buy the bike");
+            }
+            else
+            {
+                Console.WriteLine("I don't have enough money to buy the bike");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
